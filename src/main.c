5cc7c1de4ecd0582f34b4701ccab03abac489f58
Yuval Herman <yuvalherman99@gmail.com>
2026-07-20 11:20:11 +0300

    added DrawFleet function

--- src/main.c
+++ src/main.c
@@ -9,8 +9,14 @@
 #define BASE_PLANET_RADIUS 10.0f
 #define PLANET_RADIUS_GROWTH_CURVE 20.0f
 #define MAP_MARGIN 50
+#define SHIP_FONT_SIZE 20
 
 const Color planet_colors[] = {GRAY, RED, GREEN, BLUE, YELLOW};
+
+typedef struct {
+  Vector2 min_coords;
+  Vector2 max_coords;
+} GameSpace;
 
 typedef struct {
   Vector2 coords;
@@ -26,6 +32,9 @@ typedef struct {
   int dst_id;
   int total;
   int remaining;
+  // For convenience
+  Planet *src;
+  Planet *dst;
 } Fleet;
 
 typedef struct {
@@ -38,7 +47,7 @@ typedef struct {
   Fleet *items;
   size_t count;
   size_t capacity;
-} FleetDA;
+} FleetsDA;
 
 Font font;
 
@@ -47,18 +56,17 @@ void PrintPlanet(Planet planet) {
          planet.ships, planet.growth);
 }
 
+Vector2 Game2ScreenCoords(Vector2 coords, GameSpace space) {
+  return (Vector2){.x = Remap(coords.x, space.min_coords.x, space.max_coords.x,
+                              MAP_MARGIN, GetScreenWidth() - MAP_MARGIN),
+                   .y = GetScreenHeight() -
+                        Remap(coords.y, space.min_coords.y, space.max_coords.y,
+                              MAP_MARGIN, GetScreenHeight() - MAP_MARGIN)};
+}
+
 // Draws a Planet on the screen.
-// `min_coords` and `max_corrds` are the smallest and largest (x, y) coordinates
-// on the map. This values are used to make sure planets are spaced out to fill
-// the entire screen.
-void DrawPlanet(Planet planet, Vector2 min_coords, Vector2 max_coords) {
-  Vector2 draw_coords = {
-      .x = Remap(planet.coords.x, min_coords.x, max_coords.x, MAP_MARGIN,
-                 GetScreenWidth() - MAP_MARGIN),
-      .y =
-          GetScreenHeight() - Remap(planet.coords.y, min_coords.y, max_coords.y,
-                                    MAP_MARGIN, GetScreenHeight() - MAP_MARGIN),
-  };
+void DrawPlanet(Planet planet, GameSpace space) {
+  Vector2 draw_coords = Game2ScreenCoords(planet.coords, space);
 
   const float draw_radius = BASE_PLANET_RADIUS + PLANET_RADIUS_GROWTH_CURVE -
                             PLANET_RADIUS_GROWTH_CURVE / planet.growth;
@@ -75,6 +83,24 @@ void DrawPlanet(Planet planet, Vector2 min_coords, Vector2 max_coords) {
       Vector2Subtract(draw_coords, Vector2Scale(text_measurements, 0.5));
 
   DrawTextEx(font, ships_text, text_coords, font_size, spacing, BLACK);
+}
+
+void DrawFleet(Fleet fleet, GameSpace space) {
+  Vector2 draw_coords =
+      Game2ScreenCoords(Vector2Lerp(fleet.src->coords, fleet.dst->coords,
+                                    1 - (float)fleet.remaining / fleet.total),
+                        space);
+
+  const char *ships_text = TextFormat("%d", fleet.ships);
+  const float font_size = SHIP_FONT_SIZE;
+  const float spacing = 1;
+
+  const Vector2 text_measurements =
+      MeasureTextEx(font, ships_text, font_size, spacing);
+  const Vector2 text_coords =
+      Vector2Subtract(draw_coords, Vector2Scale(text_measurements, 0.5));
+  DrawTextEx(font, ships_text, text_coords, font_size, spacing,
+             planet_colors[fleet.owner]);
 }
 
 // Parses a float, advances s to the end of the parsed string, sets out to the
@@ -137,7 +163,8 @@ bool ParseMapFile(const char *map_path, PlanetDA *planets) {
     }
 
     // TODO support arbitrary amount of players, or a reasonably large number
-    if (planet.owner < 0 || planet.owner > NOB_ARRAY_LEN(planet_colors)) {
+    if (planet.owner < 0 ||
+        (size_t)planet.owner > NOB_ARRAY_LEN(planet_colors)) {
       fprintf(stderr, "Invalid number of player owned planets. The must be "
                       "between 1 to 4 players.\n");
       return false;
@@ -157,8 +184,9 @@ int main(int argc, char *argv[]) {
   const int screenHeight = 450;
 
   PlanetDA planets = {0};
-  Vector2 min_coords = {INFINITY, INFINITY},
-          max_coords = {-INFINITY, -INFINITY};
+  FleetsDA fleets = {0};
+  GameSpace game_space = {.min_coords = {INFINITY, INFINITY},
+                          .max_coords = {-INFINITY, -INFINITY}};
 
   if (argc < 2) {
     fprintf(stderr, "ERROR: No <map_file> provided.\n");
@@ -171,14 +199,24 @@ int main(int argc, char *argv[]) {
     return 1;
 
   nob_da_foreach(Planet, planet, &planets) {
-    const Vector2 coords = planet->coords;
-    min_coords = Vector2Min(planet->coords, min_coords);
-    max_coords = Vector2Max(planet->coords, max_coords);
+    game_space.min_coords = Vector2Min(planet->coords, game_space.min_coords);
+    game_space.max_coords = Vector2Max(planet->coords, game_space.max_coords);
   }
+
+  nob_da_append(&fleets, ((Fleet){
+                             .owner = 1,
+                             .ships = 23,
+                             .src_id = 1,
+                             .dst_id = 2,
+                             .total = 20,
+                             .remaining = 7,
+                             .src = &planets.items[1],
+                             .dst = &planets.items[2],
+                         }));
 
   SetTraceLogLevel(LOG_WARNING);
   SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_MSAA_4X_HINT);
-  InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+  InitWindow(screenWidth, screenHeight, "Planet Wars Viewer");
   font = GetFontDefault();
 
   SetTargetFPS(60); // Set our game to run at 60 frames-per-second
@@ -194,8 +232,10 @@ int main(int argc, char *argv[]) {
     ClearBackground(RAYWHITE);
 
     nob_da_foreach(Planet, planet, &planets) {
-      DrawPlanet(*planet, min_coords, max_coords);
+      DrawPlanet(*planet, game_space);
     }
+
+    nob_da_foreach(Fleet, fleet, &fleets) { DrawFleet(*fleet, game_space); }
 
     EndDrawing();
     //----------------------------------------------------------------------------------
