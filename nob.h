067574c54da557272cfbdb9a3b9d808bc44c3c3b
Yuval Herman <yuvalherman99@gmail.com>
2026-07-27 06:48:37 +0300

    fixed nob compile warning

--- nob.h
+++ nob.h
@@ -2501,7 +2501,7 @@ NOBDEF bool nob_read_entire_file(const char *path, Nob_String_Builder *sb)
         sb->capacity = new_count;
     }
 
-    fread(sb->items + sb->count, m, 1, f);
+    (void)!fread(sb->items + sb->count, m, 1, f);
     if (ferror(f)) {
         // TODO: Afaik, ferror does not set errno. So the error reporting in defer is not correct in this case.
         nob_return_defer(false);
