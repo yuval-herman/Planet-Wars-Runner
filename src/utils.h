2989b0b7fca9bebf4dca9f3edb7c6c1d810b468b
Yuval Herman <yuvalherman99@gmail.com>
2026-07-30 10:03:30 +0300

    replace bit index function with a compiler intrinsic
    The only reason I used such a long function for this before was for
    portability. However now, as it seems like we are committing to GCC,
    this function is much more efficient.

--- src/utils.h
+++ src/utils.h
@@ -20,22 +20,13 @@
 
 // Returns the index of the set bit in x if only one bit is set. Else return -1.
 static inline int bit_index(uint32_t x) {
-  if (x == 0 || (x & (x - 1)) != 0) {
-    return -1; // not exactly one bit set
-  }
-
-  int i = 0;
-  while ((x & 1u) == 0) {
-    x >>= 1;
-    i++;
-  }
-  return i;
+    return (x && !(x & (x - 1))) ? __builtin_ctz(x) : -1;
 }
 
 #ifdef _WIN32
-void sleep_ms(unsigned ms) { Sleep(ms); }
+static inline void sleep_ms(unsigned ms) { Sleep(ms); }
 #else
-void sleep_ms(unsigned ms) {
+static inline void sleep_ms(unsigned ms) {
   struct timespec ts;
   ts.tv_sec = ms / 1000;
   ts.tv_nsec = (long)(ms % 1000) * 1000000L;
