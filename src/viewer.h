aadc4b148eb535c9547019e2e40c60d89d6d8432
Yuval Herman <yuvalherman99@gmail.com>
2026-07-30 16:26:12 +0300

    split viewer code from main.c

---
+++ src/viewer.h
@@ -0,0 +1,20 @@
+#ifndef VIEWER_H
+#define VIEWER_H
+
+#include "game.h"
+
+#define BASE_PLANET_RADIUS 10.0f
+#define PLANET_RADIUS_GROWTH_CURVE 20.0f
+#define PLANET_RING_MAX_RADIUS 3.0F
+#define MAP_MARGIN 50
+#define CONTROLS_HEIGHT 70
+#define PLAYER_LABELS_HEIGHT 30
+#define SHIP_FONT_SIZE 20
+// The name is confusing, but this value denotes the maximum value that the
+// `game_speed` variable can hold, which controls the _lowest_ bound for game
+// speed, i.e. how slow can the game run.
+#define MAX_GAME_SPEED_VALUE 20
+
+void RunViewerForGame(GameState state);
+
+#endif // VIEWER_H
