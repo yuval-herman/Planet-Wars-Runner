9b3787e861e097134391e098cbd9536cb4388bfd
Yuval Herman <yuvalherman99@gmail.com>
2026-08-03 18:56:14 +0300

    implemented concurrent tournament runner

--- src/tournament.c
+++ src/tournament.c
@@ -1,5 +1,6 @@
 #include "tournament.h"
 
+#include <stdatomic.h>
 #include <threads.h>
 
 TournametData DeepCopyTournametData(TournametData tournament) {
@@ -21,9 +22,111 @@ void FreeInnerTournametData(TournametData tournament) {
   nob_da_free(tournament);
 }
 
-#define GetBotName(idx)                                                        \
-  (bots.items[idx].name ? bots.items[idx].name                                 \
-                        : nob_temp_sprintf("Player %u", idx + 1))
+#define sbAppendBotName(sb, idx)                                               \
+  if (match_args->bots.items[idx].name)                                        \
+    nob_sb_append_cstr(sb, match_args->bots.items[idx].name);                  \
+  else                                                                         \
+    nob_sb_appendf(sb, "Player %u", idx + 1);
+
+typedef struct {
+  unsigned p1_idx;
+  unsigned p2_idx;
+  BotsDA bots;
+  mtx_t *idx_mtx;
+  mtx_t *file_write_mtx;
+  mtx_t *tournament_data_mtx;
+  FILE *tournament_data_file;
+  const char *map_file_path;
+  TournametData *tournament_data;
+} MatchRunnerArgs;
+
+int ThrdMatchRunner(void *args) {
+  MatchRunnerArgs *match_args = args;
+  Nob_String_Builder sb = {0};
+  Nob_String_Builder temp_sb = {0};
+  BotsDA playing_bots = {0};
+  playing_bots.count = 2;
+  playing_bots.capacity = playing_bots.count;
+  playing_bots.items = malloc(sizeof *playing_bots.items * playing_bots.count);
+
+  while (true) {
+    sb.count = 0;
+    temp_sb.count = 0;
+    mtx_lock(match_args->idx_mtx);
+    const unsigned p1_idx = match_args->p1_idx;
+    const unsigned p2_idx = match_args->p2_idx;
+
+    if (match_args->p2_idx == match_args->bots.count - 1) {
+      if (match_args->p1_idx < match_args->bots.count - 2) {
+        match_args->p1_idx++;
+        match_args->p2_idx = match_args->p1_idx + 1;
+      } else {
+        mtx_unlock(match_args->idx_mtx);
+        nob_da_free(playing_bots);
+        nob_sb_free(sb);
+        nob_sb_free(temp_sb);
+        return 0;
+      }
+    } else {
+      match_args->p2_idx++;
+    }
+    mtx_unlock(match_args->idx_mtx);
+
+    nob_sb_append_cstr(&sb, "match:\n");
+    sbAppendBotName(&sb, p1_idx);
+    nob_sb_append(&sb, '\n');
+    sbAppendBotName(&sb, p2_idx);
+    nob_sb_append(&sb, '\n');
+
+    playing_bots.items[0] = match_args->bots.items[p1_idx];
+    playing_bots.items[1] = match_args->bots.items[p2_idx];
+    // nob_log(NOB_INFO, "Running match between %s and %s", GetBotName(p1_idx),
+    //         GetBotName(p2_idx));
+
+    // Run a full game, silence normal logging so we don't clog the terminal
+    // TODO split the `MakeGame` function to more sub-functions so we can load
+    // maps, bots and other stuff once instead of on every match.
+    GameState state = MakeGame(match_args->map_file_path, playing_bots, false);
+    RunGame(&state);
+
+    GameLog game_log_copy = DeepCopyGameLog(state.game_log);
+
+    if (game_log_copy.draw) {
+      // nob_log(NOB_INFO, "Game ended in a draw");
+      nob_sb_append_cstr(&sb, "draw\n");
+    } else {
+      unsigned winner_idx = game_log_copy.winning_bot == 0 ? p1_idx : p2_idx;
+      // nob_log(NOB_INFO, "%s won.", GetBotName(winner_idx));
+      nob_sb_append_cstr(&sb, "winner: ");
+      sbAppendBotName(&sb, winner_idx);
+      nob_sb_append(&sb, '\n');
+    }
+
+    temp_sb.count = 0;
+    nob_sb_append_cstr(&temp_sb, "tournament/match_");
+    sbAppendBotName(&temp_sb, p1_idx);
+    nob_sb_append(&temp_sb, '-');
+    sbAppendBotName(&temp_sb, p2_idx);
+    nob_sb_append_cstr(&temp_sb, ".plws");
+    sb_append_null(&temp_sb);
+
+    char *save_file_name = temp_sb.items;
+    nob_sb_appendf(&sb, "save file name: %s\n", save_file_name);
+
+    FILE *save_file = fopen(save_file_name, "wb");
+    WriteGameLogToFile(save_file, state.game_log);
+    fclose(save_file);
+
+    mtx_lock(match_args->tournament_data_mtx);
+    nob_da_append(match_args->tournament_data, game_log_copy);
+    mtx_unlock(match_args->tournament_data_mtx);
+    FreeInnerGameState(state);
+
+    mtx_lock(match_args->file_write_mtx);
+    fwrite(sb.items, 1, sb.count, match_args->tournament_data_file);
+    mtx_unlock(match_args->file_write_mtx);
+  }
+}
 
 TournametData RunTournament(const char *map_file_path, BotsDA bots) {
   if (bots.count < 3) {
@@ -44,58 +147,47 @@ TournametData RunTournament(const char *map_file_path, BotsDA bots) {
   }
 
   TournametData tournament = {0};
-  BotsDA playing_bots = {0};
-  playing_bots.count = 2;
-  playing_bots.capacity = playing_bots.count;
-  playing_bots.items = malloc(sizeof *playing_bots.items * playing_bots.count);
 
-  unsigned match_num = 0;
-  for (unsigned p1_idx = 0; p1_idx < bots.count - 1; p1_idx++) {
-    for (unsigned p2_idx = p1_idx + 1; p2_idx < bots.count; p2_idx++) {
-      size_t mark = nob_temp_save();
-      match_num++;
-      fprintf(tournament_data_file, "match %u:\n%s\n%s\n", match_num,
-              GetBotName(p1_idx), GetBotName(p2_idx));
+  // Could be nob_nprocs()-1 because we use the current thread to manage it all,
+  // but this thread doesn't do a lot of work, mainly waits around, so I think
+  // it fine that way.
+  const unsigned thread_count = nob_nprocs();
+  thrd_t *thread_pool = malloc(sizeof *thread_pool * thread_count);
 
-      playing_bots.items[0] = bots.items[p1_idx];
-      playing_bots.items[1] = bots.items[p2_idx];
-      nob_log(NOB_INFO, "Running match between %s and %s", GetBotName(p1_idx),
-              GetBotName(p2_idx));
+  mtx_t file_write_mtx;
+  mtx_init(&file_write_mtx, mtx_plain);
+  mtx_t tournament_data_mtx;
+  mtx_init(&tournament_data_mtx, mtx_plain);
+  mtx_t idx_mtx;
+  mtx_init(&idx_mtx, mtx_plain);
 
-      // Run a full game, silence normal logging so we don't clog the terminal
-      // TODO split the `MakeGame` function to more sub-functions so we can load
-      // maps, bots and other stuff once instead of on every match.
-      GameState state = MakeGame(map_file_path, playing_bots, false);
-      nob_minimal_log_level = NOB_WARNING;
-      RunGame(&state);
-      nob_minimal_log_level = NOB_INFO;
+  MatchRunnerArgs thread_args = {
+      .p1_idx = 0,
+      .p2_idx = 1,
+      .idx_mtx = &idx_mtx,
+      .bots = bots,
+      .file_write_mtx = &file_write_mtx,
+      .map_file_path = map_file_path,
+      .tournament_data = &tournament,
+      .tournament_data_mtx = &tournament_data_mtx,
+      .tournament_data_file = tournament_data_file,
+  };
 
-      GameLog game_log_copy = DeepCopyGameLog(state.game_log);
-
-      if (game_log_copy.draw) {
-        nob_log(NOB_INFO, "Game ended in a draw");
-        fprintf(tournament_data_file, "draw\n");
-      } else {
-        unsigned winner_idx = game_log_copy.winning_bot == 0 ? p1_idx : p2_idx;
-        nob_log(NOB_INFO, "%s won.", GetBotName(winner_idx));
-        fprintf(tournament_data_file, "winner: %s\n", GetBotName(winner_idx));
-      }
-
-      char *save_file_name = nob_temp_sprintf("match_%u.plws", match_num);
-      fprintf(tournament_data_file, "save file name: %s\n", save_file_name);
-
-      FILE *save_file =
-          fopen(nob_temp_sprintf("tournament/%s", save_file_name), "wb");
-      WriteGameLogToFile(save_file, state.game_log);
-      fclose(save_file);
-
-      nob_da_append(&tournament, game_log_copy);
-      FreeInnerGameState(state);
-      nob_temp_rewind(mark);
-    }
+  // All normal logging must stop as this functions are not thread-safe
+  nob_minimal_log_level = NOB_WARNING;
+  for (unsigned i = 0; i < thread_count; i++) {
+    thrd_create(&thread_pool[i], ThrdMatchRunner, &thread_args);
   }
-  fclose(tournament_data_file);
+  for (unsigned i = 0; i < thread_count; i++) {
+    thrd_join(thread_pool[i], NULL);
+  }
+  nob_minimal_log_level = NOB_INFO;
 
-  nob_da_free(playing_bots);
+  fclose(tournament_data_file);
+  free(thread_pool);
+  mtx_destroy(&file_write_mtx);
+  mtx_destroy(&tournament_data_mtx);
+  mtx_destroy(&idx_mtx);
+
   return tournament;
 }
