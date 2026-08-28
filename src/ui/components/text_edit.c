758f94bee7c5f292ed0f9ab87ac2055bc558dceb
Yuval Herman <yuvalherman99@gmail.com>
2026-08-28 04:52:35 +0300

    renamed keyboard cursor to caret for clarity

--- src/ui/components/text_edit.c
+++ src/ui/components/text_edit.c
@@ -4,17 +4,17 @@
 
 #include "../ui_utils.h"
 
-// Displays an editable text box.
+// Displays an editable text box. Only a single line is supported.
 // If `sb` contains some data prior to calling this function, it will be
 // displayed in the text box.
 // The return value is a clay string pointing to the `sb` data for convenience.
-Clay_String Component_TextEdit(Nob_String_Builder *sb, unsigned *cursor_pos,
+Clay_String Component_TextEdit(Nob_String_Builder *sb, unsigned *caret_pos,
                                bool *is_active);
 
 // Don't include implementation when included from the componenets header file
 #ifndef COMPONENTS_H
 
-#define CURSOR_COLOR ((Clay_Color){255, 255, 255, 127})
+#define CARET_COLOR ((Clay_Color){255, 255, 255, 127})
 
 static void onHover(Clay_ElementId element_id, Clay_PointerData pointer_data,
                     void *user_data) {
@@ -26,7 +26,7 @@ static void onHover(Clay_ElementId element_id, Clay_PointerData pointer_data,
   }
 }
 
-static inline void SetCursorByHover() {
+static inline void SetCursorShapeByHover() {
   if (Clay_Hovered()) {
     if (GetMouseCursor() != MOUSE_CURSOR_IBEAM)
       SetMouseCursor(MOUSE_CURSOR_IBEAM);
@@ -36,7 +36,7 @@ static inline void SetCursorByHover() {
   }
 }
 
-Clay_String DrawTextEdit(Nob_String_Builder sb, unsigned *cursor_pos,
+Clay_String DrawTextEdit(Nob_String_Builder sb, unsigned *caret_pos,
                          bool *is_focused) {
   const Clay_String str = SB_TO_CLAY(sb);
   const int inner_padding = 8;
@@ -48,22 +48,22 @@ Clay_String DrawTextEdit(Nob_String_Builder sb, unsigned *cursor_pos,
     },
     .backgroundColor = *is_focused ? CLAY_COLOR(BLUE) : CLAY_COLOR(GRAY)
   }) {
-    SetCursorByHover();
+    SetCursorShapeByHover();
     Clay_OnHover(onHover, is_focused);
     Clay_TextElementConfig text_conf = {.fontId = 1, .fontSize = 32, .textColor = CLAY_COLOR(BLACK)};
     CLAY_TEXT(str, text_conf);
     if (*is_focused) {
-      CLAY(CLAY_ID_LOCAL("Cursor"), {
+      CLAY(CLAY_ID_LOCAL("Caret"), {
         .floating = {
           .pointerCaptureMode = CLAY_POINTER_CAPTURE_MODE_PASSTHROUGH,
           .attachTo = CLAY_ATTACH_TO_PARENT,
           .attachPoints = {CLAY_ATTACH_POINT_LEFT_CENTER, CLAY_ATTACH_POINT_LEFT_CENTER},
-          .offset = {.y = 0, .x = inner_padding + *cursor_pos * 16}
+          .offset = {.y = 0, .x = inner_padding + *caret_pos * 16}
         },
         .layout = {
           .sizing = {.height = CLAY_SIZING_PERCENT(0.9), .width = CLAY_SIZING_FIXED(4)},
         },
-        .backgroundColor = CURSOR_COLOR
+        .backgroundColor = CARET_COLOR
       }) {}
     }
   }
@@ -71,23 +71,23 @@ Clay_String DrawTextEdit(Nob_String_Builder sb, unsigned *cursor_pos,
   return str;
 }
 
-Clay_String Component_TextEdit(Nob_String_Builder *sb, unsigned *cursor_pos,
+Clay_String Component_TextEdit(Nob_String_Builder *sb, unsigned *caret_pos,
                                bool *is_focused) {
   if (*is_focused) {
-    if (IsKeyPressed(KEY_RIGHT) && *cursor_pos < sb->count)
-      (*cursor_pos)++;
-    if (IsKeyPressed(KEY_LEFT) && *cursor_pos > 0)
-      (*cursor_pos)--;
+    if (IsKeyPressed(KEY_RIGHT) && *caret_pos < sb->count)
+      (*caret_pos)++;
+    if (IsKeyPressed(KEY_LEFT) && *caret_pos > 0)
+      (*caret_pos)--;
 
-    if (IsKeyPressed(KEY_BACKSPACE) && *cursor_pos > 0) {
-      for (unsigned i = *cursor_pos - 1; i < sb->count; i++) {
+    if (IsKeyPressed(KEY_BACKSPACE) && *caret_pos > 0) {
+      for (unsigned i = *caret_pos - 1; i < sb->count; i++) {
         sb->items[i] = sb->items[i + 1];
       }
       sb->count--;
-      (*cursor_pos)--;
+      (*caret_pos)--;
     }
-    if (IsKeyPressed(KEY_DELETE) && *cursor_pos < sb->count) {
-      for (unsigned i = *cursor_pos; i < sb->count; i++) {
+    if (IsKeyPressed(KEY_DELETE) && *caret_pos < sb->count) {
+      for (unsigned i = *caret_pos; i < sb->count; i++) {
         sb->items[i] = sb->items[i + 1];
       }
       sb->count--;
@@ -100,16 +100,16 @@ Clay_String Component_TextEdit(Nob_String_Builder *sb, unsigned *cursor_pos,
       if (key >= 32 && key <= 126) {
         nob_da_reserve(sb, sb->count + 1);
 
-        for (unsigned i = sb->count; i > *cursor_pos; i--) {
+        for (unsigned i = sb->count; i > *caret_pos; i--) {
           sb->items[i] = sb->items[i - 1];
         }
         sb->count++;
-        sb->items[(*cursor_pos)++] = (char)key;
+        sb->items[(*caret_pos)++] = (char)key;
       }
 
       key = GetCharPressed(); // Check next character in the queue
     }
   }
-  return DrawTextEdit(*sb, cursor_pos, is_focused);
+  return DrawTextEdit(*sb, caret_pos, is_focused);
 }
 #endif // COMPONENTS_H
